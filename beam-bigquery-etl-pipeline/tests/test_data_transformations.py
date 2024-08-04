686aa56e1489208e2c291e0c656395334369eebf
Pano Pouroullis <53717261+Panolof@users.noreply.github.com>
2024-08-04 23:15:18 +0100

    Add initial test for data transformations

---
+++ beam-bigquery-etl-pipeline/tests/test_data_transformations.py
@@ -0,0 +1,10 @@
+import unittest
+from src.utils.data_transformations import filter_transactions
+
+class TestDataTransformations(unittest.TestCase):
+    def test_filter_transactions(self):
+        # Test to be implemented
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
